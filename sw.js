261c50572fded686ba778713b563e13a96c3f523
pedrojsoftware-web <pedrojsoftware@gmail.com>
2026-03-12 21:27:23 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,17 @@
-const CACHE_NAME = 'anestesia-pedia-v2';
+const CACHE_NAME = 'anestesia-pedia-v3';
+// Get the repository name from the URL path.
+// For example, from https://username.github.io/anestesia-pediatrica/
+// it extracts '/anestesia-pediatrica/'
+const basePath = self.location.pathname.substring(0, self.location.pathname.lastIndexOf('/') + 1);
+
 const urlsToCache = [
-  './',
-  'index.html',
-  'style.css',
-  'app.js',
-  'manifest.json',
-  'icons/icon-192x192.png',
-  'icons/icon-512x512.png'
+  basePath,
+  basePath + 'index.html',
+  basePath + 'style.css',
+  basePath + 'app.js',
+  basePath + 'manifest.json',
+  basePath + 'icons/icon-192x192.png',
+  basePath + 'icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
